a1613789cf8b5276029e71eda99138fb2c7a4243
AbiMani <aby_viana@zoho.com>
2021-07-05 17:07:34 -0600

    Creacion de endpoints

---
+++ app/Http/Controllers/ReporteController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Reporte;
+use Illuminate\Http\Request;
+
+class ReporteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Reporte::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $reporte=Reporte::create($request->all());
+
+        return response()->json($reporte,201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Reporte $reporte)
+    {
+        //
+        return $reporte;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Reporte $reporte)
+    {
+        //
+        $reporte->update($request->all);
+
+        return response()->json($reporte, 200);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Reporte $reporte)
+    {
+        //
+        $reporte->delete();
+        return response()->json(null,204);
+    }
+}
